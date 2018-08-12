11a223ef5c4de1f9b67491aa597bc0720aea717c
D-CURT <36221315+D-CURT@users.noreply.github.com>
2018-08-12 16:14:15 +0300

    UI added updated;

--- src/Runner.java
+++ src/Runner.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.util.*;
 
 public class Runner {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         RandomGenerator generator = new RandomGenerator();
         Queue<Integer> queue;
         int quantity, min, max;
